85114a0ceff2ac15e12a8ea0eb5f210863e5f3c7
Asalagi <mypaintedfrog@yahoo.com>
2023-05-17 08:34:48 -0500

    setting up first file of code-drills

---
+++ typing-drills.js
@@ -0,0 +1,11 @@
+const num1 = num2 = 10;
+for (let i = num1; i <= num2; i++) {
+    setTimeout(() => console.log(i), i * 1000);
+}
+
+const num1 = 1, num2 = 10;
++function displayWithDelay(i){
+    console.log(i);
+    if(i !== num2)
+        setTimeout(displayWithDelat, 1000, ++i);
+}(1);
