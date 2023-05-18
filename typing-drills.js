cafc7ea6deae16b91acd3fd5e6c7fa8fdb64bd8d
Asalagi <mypaintedfrog@yahoo.com>
2023-05-18 10:15:26 -0500

    day two of typing drill

--- typing-drills.js
+++ typing-drills.js
@@ -1,11 +1,31 @@
-const num1 = num2 = 10;
-for (let i = num1; i <= num2; i++) {
-    setTimeout(() => console.log(i), i * 1000);
-}
+const num1 = 1, num2 = 10;
+let i = num1;
+const intervalId = setInterval(() => {
+    console.log(i++);
+    if (i === num2 + 1)
+        clearInterval(intervalId);
+}, 1000);
 
 const num1 = 1, num2 = 10;
-+function displayWithDelay(i){
-    console.log(i);
-    if(i !== num2)
-        setTimeout(displayWithDelay, 1000, ++i);
-}(1);
+let i = num1;
+const intervalId = setInterval(() => {
+    console.log(i++);
+    if (i === num2 + 1)
+        clearInterval(intervalId);
+}, 1000);
+
+const num1 = 1, num2 = 10;
+let i = num1;
+const intveralId = setInterval(() => {
+    console.log(i++);
+    if (i === num2 + 1)
+        clearInterval(intervalId);
+}, 1000);
+
+const num1 = 1, num2 = 10;
+let i = num1;
+const intervalId = setInterval(() => {
+    console.log(i++)
+    if (i === num2 + 1)
+        clearInterval(intervalId);
+}, 1000);
