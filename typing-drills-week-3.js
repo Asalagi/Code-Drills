98613ad919959a21a25fcf968908fc03ae8fcfdf
Asalagi <mypaintedfrog@yahoo.com>
2023-06-01 08:42:23 -0500

    adding day 3 to typing week 3

--- typing-drills-week-3.js
+++ typing-drills-week-3.js
@@ -48,66 +48,66 @@ asyncFunc1()
 .then(asyncFunc3)
 .catch(() => { console.log("Error occured in one of the async function"); });
 
+// Day Three
+
++async function executor() {
+    try {
+        await asyncFunc1();
+        await asyncFunc2();
+        await asyncFunc3();
+        console.log('All succeeded');
+    }
+    catch(){
+        console.log("Error occured");
+    }
+}();
+
 //
 
-asyncFunc1().then(
-    () => {
-        console.log('Completed async1');
-        asyncFunc2().then(
-            () => {
-                console.log('Completed async2');
-                asyncFunc3().then(
-                    () => {
-                        console.log('Completed async3');
-                        console.log('All succeeded');
-                    },
-                    (err) => {
-                        console.log('Failure in ' + err);
-                    },
-                );
-            },
-            (err) => {
-                console.log('Failure in ' + err);
-            }
-        );
-    },
-    (err) => {
-        console.log('Failure in ' + err);
++async function executor() {
+    try {
+        await asyncFunc1();
+        await asyncFunc2();
+        await asyncFunc3();
+        console.log('All succeeded');
     }
-);
-
-asyncFunc1()
-.then(asyncFunc2)
-.then(asyncFunc3)
-.catch(() => { console.log("Error occured in one of the async function"); });
-
-asyncFunc1().then(
-    () => { 
-        console.log('Completed async1');
-        asyncFunc2().then(
-            () => {
-                console.log('Completed async2');
-                asyncFunc3().then(
-                    () => {
-                        console.log('ZCompleted async3');
-                        console.log('All Succeeded');
-                    },
-                    (err) => {
-                        console.log('Failure in ' + err);
-                    }
-                );
-            },
-            (err) => { 
-                console.log('Failute in ' + err);
-            }
-        );
-    },
-    (err) => {
-        console.log('Failure in ' + err);
+    catch(){
+        console.log("Error occuted");
     }
-);
+}();
 
-asyncFunc1()
-.then(asyncFunc2)
-.then(asyncFunc3)
-.catch(() => { console.log("Error occured in one of the async function"); });
++async function executor() {
+    try {
+        await asyncFunc1();
+        await aynceFunc2();
+        await asyncFunc3();
+        console.log('All succeeded');
+    }
+    catch() {
+        console.log("Error occured");
+    }
+}();
+
++async function executor(){
+    try{
+        await asyncFunc1();
+        await asyncFunc2();
+        await asyncFunc3();
+        console.log('All succeeded');
+    }
+    catch(){
+        console.log("Error occured");
+    }
+}();
+
++aynce function executor(){
+    try {
+        await asyncFunc1();
+        await asyncFunc2();
+        await asyncFunc3();
+        console.log('All succeeded');
+    }
+    catch(){
+        console.log("Error occured");
+    }
+}();
