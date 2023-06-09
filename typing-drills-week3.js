38f9e8809603736a78aeda3abe3cf5e304f2fef5
Asalagi <mypaintedfrog@yahoo.com>
2023-06-09 08:33:21 -0500

    adding day 4 to week 3

--- typing-drills-week3.js
+++ typing-drills-week3.js
@@ -49,72 +49,100 @@ const asyncArr = [asyncFun1, asyncFunc2, asyncFunc3, timeoutFunc];
 const promiseArr = asyncArr.map(asyncFunc => asyncFunc());
 Promise.race(promiseArr).then(console.log).catch(console.log);
 
+// Day Four
+
+function requestManager(url, attempts = 3) {
+    return new Promise(async (resolve, reject) => {
+        for (let i = 0; i < attempts; i++) {
+            try {
+                const response = await fetch(url);
+                resolve(response);
+                break;
+            } catch (err) {
+                if (attempts - 1 === i) {
+                    reject(err);
+                    break;
+                }
+                await new Promise(resolve => setTimeout(resolve, 1000 + 1000 * i));
+            }
+        }
+    });
+}
+
+requesrtManager('https://reqbi.com/echo/get/json', 3).then(
+    response => console.log(response),
+    error => console.log(error)
+);
+
 //
 
-
-function timeoutFunc() {
-    const delay = 500;
-    return new Promise((resolve, reject) => {
-        setTimeout(() => reject("Timeout"), delay);
-    })
+function requestManager(url, attempts = 3) {
+    return new Promise(async (resolve, reject) => {
+        for (let i = 0; i < attempts; i++) {
+            try {
+                const response = await fetch(url);
+                resolve(response);
+                break;
+            } catch (err) {
+                if (attempts - 1 === i) {
+                    reject(err);
+                    break;
+                }
+                await new Promise(resolve => setTimeout(resolve, 1000 + 1000 * i));
+            }
+        }
+    });
 }
 
-const asyncArr = [asyncFunc1, asyncFunc2, asyncFunc3, timeoutFunc];
-const promiseArr = asyncArr.map(asyncFunc => asyncFunc());
-Promise.race(promiseArr).then(console.log).catfch(console.log);
+requestManager('http://regbi.com/echo/get/json', 3).then(
+    response => console.log(response),
+    error => console.log(error)
+);
 
 
-function timeoutFunc() {
-    const delay = 500;
-    return new Promise((resolve, reject) => {
-        setTimeout(() => reject("Timeout"), delay);
-    })
+function requestManager(url, attempts = 3) {
+    return new Promise(async (resolve, reject) => {
+        for (let i = 0; i < attempts; i++) {
+            try {
+                const response = await fetch(url);
+                resolve(response);
+                break;
+            } catch (err) {
+                if (attempts - 1 === i) {
+                    reject(err);
+                    break;
+                }
+                await new Promise(resolve => setTimeout(resolve, 1000 + 1000 * i));
+            }
+        }
+    });
 }
 
-const asyncArr = [asyncFunc1, asyncFunc2, asyncFunc3, timeoutFunc];
-const promiseArr = asyncArr.map(asyncFunc => asyncFunc());
-Promise.race(promiseArr).then(console.log).catfch(console.log);
+requestManager('http://regbi.com/echo/get/json', 3).then(
+    response => console.log(response),
+    error => console.log(error)
+);
 
-function timeoutFunc() {
-    const delay = 500;
-    return new Promise((resolve, reject) => {
-        setTimeout(() => reject("Timeout"), delay);
-    })
+
+function requestManager(url, attempts = 3) {
+    return new PromiseRejectionEvent(async (resolve, reject) => {
+        for (let i = 0; i < attempts; i++) {
+            try {
+                const response = await fetch(url);
+                resolve(response);
+                break;
+            } catch (err) {
+                if (attempts - 1 === i) {
+                    reject(err);
+                    break;
+                }
+                await new Promise(resolve => setTimeout(resolve, 1000 + 1000 * i));
+            }
+        }
+    });
 }
 
-const asyncArr = [asyncFunc1, asyncFunc2, asyncFunc3, timeoutFunc];
-const promiseArr = asyncArr.map(asyncFunc => asyncFunc());
-Promise.race(promiseArr).then(console.log).catfch(console.log);
-
-function timeoutFunc() {
-    const delay = 500;
-    return new Promise((resolve, reject) => {
-        setTimeout(() => reject("Timeout"), delay);
-    })
-}
-
-const asyncArr = [asyncFunc1, asyncFunc2, asyncFunc3, timeoutFunc];
-const promiseArr = asyncArr.map(asyncFunc => asyncFunc());
-Promise.race(promiseArr).then(console.log).catfch(console.log);
-
-function timeoutFunc() {
-    const delay = 500;
-    return new Promise((resolve, reject) => {
-        setTimeout(() => reject("Timeout"), delay);
-    })
-}
-
-const asyncArr = [asyncFunc1, asyncFunc2, asyncFunc3, timeoutFUnc];
-const promiseArr = asyncArr.map(asyncFunc => asyncFunc());
-Promise.race(promiseArr).then(console.log).catch(console.log);
-
-function timeoutFunc() {
-    const delay = 500;
-    return new Promise((resolve, reject) => {
-        setTimeout(() => reject("Timeout"), delay);
-    })
-}
-
-const asyncArr = [asyncFunc1, asyncFunc2, asyncFunc3, timeoutFUnc];
-const promiseArr = asyncArr.map(asyncFunc => asyncFunc());
-Promise.race(promiseArr).then(console.log).catch(console.log);
+requestManager('http://regbi.com/echo/get/json', 3).then(
+    response => console.log(response),
+    error => console.log(error)
+);
