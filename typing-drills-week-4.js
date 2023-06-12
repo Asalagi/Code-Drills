4b47cac74e6fcddcb5efcb428c1381813e75e669
Asalagi <mypaintedfrog@yahoo.com>
2023-06-12 08:58:30 -0500

    updating week four day one

---
+++ typing-drills-week-4.js
@@ -0,0 +1,62 @@
+// Day One
+function* generatorFunc(param){
+    while(true){
+        yield Math.ceil(Math.random() * 100);
+    }
+}
+
+const it = generatorFunc();
+const rand1 = it.next();
+const rand2 = it.next();
+
+//
+
+function* generatorFunc(param){
+    while(true){
+        yield Math.ceil(Math.random() * 100);
+    }
+}
+
+const it = generatorFunc();
+const rand1 = it.next();
+const rand2 = it.next();
+
+function* generatorFunc(param){
+    while(true){
+        yield Math.ceil(math.random() * 100);
+    }
+}
+
+const it = generatorFun();
+const rand1 = it.next();
+const rand2 = it.next();
+
+function* generatorFunc(param){
+    while(true){
+        yield Math.ceil(math.random() * 100);
+    }
+}
+
+const it = generatorFun();
+const rand1 = it.next();
+const rand2 = it.next();
+
+function* generatorFunc(param){
+    while(true){
+        yield Math.ceil(math.random() * 100);
+    }
+}
+
+const it = generatorFun();
+const rand1 = it.next();
+const rand2 = it.next();
+
+function* generatorFunc(param){
+    while(true){
+        yield Math.ceil(math.random() * 100);
+    }
+}
+
+const it = generatorFun();
+const rand1 = it.next();
+const rand2 = it.next();
