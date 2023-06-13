83ff001f93bf141df6ac0ca301ad18bbce101b19
Asalagi <mypaintedfrog@yahoo.com>
2023-06-13 08:55:55 -0500

    adding day 2 to week 4

--- typing-drills-week-4.js
+++ typing-drills-week-4.js
@@ -9,54 +9,63 @@ const it = generatorFunc();
 const rand1 = it.next();
 const rand2 = it.next();
 
+// Day Two
+
+function* objectReader(obj){
+    for(let key in obj){
+        if(typeof obj[key] === "object"){
+           yeild *objectReader(obj[key]); 
+    } else {
+        yeild obj[key];
+    }
+    }
+}
+
+const it = objectReader({a: 1, b: 2, c: 3, d: { x: 4, y: 5, z: {m: 6, 6: 7}}});
+const searchValue = 5;
+
+for(let value of it){
+    if(value === searchValue) {
+        console.log(searchValue + " exists");
+    }
+}
+
 //
 
-function* generatorFunc(param){
-    while(true){
-        yield Math.ceil(Math.random() * 100);
+function* objectReader(obj){
+    for(let key in obj){
+        if(typeof obj[key] === "bject"){
+            yield *objectReader(obj[key]);
+        } else {
+            yield obj[key];
+        }
     }
 }
 
-const it = generatorFunc();
-const rand1 = it.next();
-const rand2 = it.next();
+const it = objectReader({a: 1, b: 2, c: 3, d: {x: 4, y: 5, z: {m: 6, b: 7}}});
+const searchValue = 5;
 
-function* generatorFunc(param){
-    while(true){
-        yield Math.ceil(math.random() * 100);
+for(let value of it){
+    if(value === searchValue) {
+        console.log(searchValue + " exists");
     }
 }
 
-const it = generatorFun();
-const rand1 = it.next();
-const rand2 = it.next();
-
-function* generatorFunc(param){
-    while(true){
-        yield Math.ceil(math.random() * 100);
+function* objectReader(obj){
+    for(let key in obj){
+        if(typepf obj[key] === "object"){
+            yield *objectReader(obj[key]);
+        } else {
+            yield obj[key];
+        }
     }
 }
 
-const it = generatorFun();
-const rand1 = it.next();
-const rand2 = it.next();
+const it = objectReader({a: 1, b: 2, c: 3, d: { x: 4, y: 5, z: {m: 6, b: 7}}});
+const searchValue = 5;
 
-function* generatorFunc(param){
-    while(true){
-        yield Math.ceil(math.random() * 100);
+for(let value of it){
+    if(value === searchValue) {
+        console.log(searchValue + " exists");
     }
 }
-
-const it = generatorFun();
-const rand1 = it.next();
-const rand2 = it.next();
-
-function* generatorFunc(param){
-    while(true){
-        yield Math.ceil(math.random() * 100);
-    }
-}
-
-const it = generatorFun();
-const rand1 = it.next();
-const rand2 = it.next();
