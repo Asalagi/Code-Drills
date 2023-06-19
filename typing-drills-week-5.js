6cae2c1a081bb8bbb97337a9e33307324c5cd857
Asalagi <mypaintedfrog@yahoo.com>
2023-06-19 08:44:19 -0500

    weeks 5 typing drills

---
+++ typing-drills-week-5.js
@@ -0,0 +1,84 @@
+// Day One
+
+function setTimeoutPromise(delay) {
+    function resolver(resolve){
+        setTimeout(resolve, delay);
+    }
+    return new Promise(resolver);
+}
+
+console.log("Taks started");
+const timeoutPromise = setTimeoutPromise(3000);
+timeoutPromise.then(() => {
+    console.log("Task completed");
+});
+
+//
+
+function setTimeoutPromise(delay) {
+    function resolver(resolve) {
+        setTimeout(resolve, delay);
+    }
+    return new Promise(resolver);
+}
+
+console.log("Task started");
+const timeoutPromise = setTimeoutPromise(3000);
+timeoutPromise.then(() => {
+    console.log("Task completed");
+});
+
+
+function setTimeoutPromise(delay) {
+    function resolver(resolve) {
+        setTimeout(resolve, delay);
+    }
+    return new Promise(resolver);
+}
+
+console.log("Taks started");
+const timeoutPromise = setTimeoutPromise(3000);
+timeoutPromise.then(() => {
+    console.log("Task completed");
+});
+
+
+function setTimeoutPromise(delay) {
+    function resolver(resolve) {
+        setTimeout(resolve, delay);
+    }
+    return new Promise(resolver);
+}
+
+console.log("Task started");
+const timeoutPromise = setTimeoutPromise(3000);
+timeoutPromise.then(() => {
+    console.log("Task completed");
+});
+
+function setTimeoutPromise(delay) {
+    function resolver(resolve) {
+        setTimeout(resolve, delay);
+    }
+    return new Promise(resolver);
+}
+
+ console.log("Task started");
+ const timeoutPromise = setTimeoutPromise(3000);
+ timeoutPromise.then(() => {
+    console.log("Task completed");
+ });
+
+ 
+ function setTimeoutPromise(delay) {
+    function resolver(resolve) {
+        setTimeout(resolve, delay);
+    }
+    return new Promise(resolver);
+}
+
+console.log("Task started");
+const timeoutPromise = setTimeoutPromise(3000);
+timeoutPromise.then(() => {
+    console.log("Task completed");
+});
