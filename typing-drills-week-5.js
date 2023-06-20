ec4a49098c930e6a8095f6bdff7f0c3b676a550b
Asalagi <mypaintedfrog@yahoo.com>
2023-06-20 08:43:01 -0500

    adding day 2 to week 5

--- typing-drills-week-5.js
+++ typing-drills-week-5.js
@@ -13,72 +13,61 @@ timeoutPromise.then(() => {
     console.log("Task completed");
 });
 
+// Day Two
+
+function fetchData(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("Get", url);
+        xhr.onload = function () {
+            try {
+                if (this.status === 200) {
+                    resolve(this);
+                } else {
+                    reject(this);
+                }
+            } catch (e) {
+                reject(e);
+            }
+        };
+        xhr.onerror = function () {
+            reject(this);
+        };
+        xhr.send();
+    });
+}
+
+fetchData("http://reqbin.com/echo/get/json")
+.then((data) => {
+    console.log(data);
+})
+.catch((err) => console.log(err));
+
 //
 
-function setTimeoutPromise(delay) {
-    function resolver(resolve) {
-        setTimeout(resolve, delay);
-    }
-    return new Promise(resolver);
+function fetchData(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = function () {
+            try {
+                if (this.status === 200) {
+                    resolve(this);
+                } else {
+                    reject(this);
+                }
+            } catch (e) {
+                reject(e);
+            }
+        };
+        xhr.onerror = function () {
+            reject(this);
+        };
+    });
 }
 
-console.log("Task started");
-const timeoutPromise = setTimeoutPromise(3000);
-timeoutPromise.then(() => {
-    console.log("Task completed");
-});
-
-
-function setTimeoutPromise(delay) {
-    function resolver(resolve) {
-        setTimeout(resolve, delay);
-    }
-    return new Promise(resolver);
-}
-
-console.log("Taks started");
-const timeoutPromise = setTimeoutPromise(3000);
-timeoutPromise.then(() => {
-    console.log("Task completed");
-});
-
-
-function setTimeoutPromise(delay) {
-    function resolver(resolve) {
-        setTimeout(resolve, delay);
-    }
-    return new Promise(resolver);
-}
-
-console.log("Task started");
-const timeoutPromise = setTimeoutPromise(3000);
-timeoutPromise.then(() => {
-    console.log("Task completed");
-});
-
-function setTimeoutPromise(delay) {
-    function resolver(resolve) {
-        setTimeout(resolve, delay);
-    }
-    return new Promise(resolver);
-}
-
- console.log("Task started");
- const timeoutPromise = setTimeoutPromise(3000);
- timeoutPromise.then(() => {
-    console.log("Task completed");
- });
-
- 
- function setTimeoutPromise(delay) {
-    function resolver(resolve) {
-        setTimeout(resolve, delay);
-    }
-    return new Promise(resolver);
-}
-
-console.log("Task started");
-const timeoutPromise = setTimeoutPromise(3000);
-timeoutPromise.then(() => {
-    console.log("Task completed");
-});
+fetchData("https://reqbin.com/echo/get/json")
+.then((data) => {
+    console.log(data);
+})
+.catch((err) => console.log(err));
