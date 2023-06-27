a253a597bb9d79fc876cbdf1529d3b0b6784dcd7
Asalagi <mypaintedfrog@yahoo.com>
2023-06-27 08:54:26 -0500

    adding day two to week six

--- typing-drills-week-6.js
+++ typing-drills-week-6.js
@@ -1,3 +1,4 @@
+// Day One
 const asyncArr = [async1, async2, async3];
 const promiseArr = asyncArr.map((async) => async());
 finalResolution = Promise.all(promiseArr);
@@ -23,53 +24,72 @@ const promiseArr = asyncArr.map((async) => async());
     }
 })();
 
+//Day Two
+
+const asyncArr = [async1, async2, async3];
+const promiseArr = asyncArr.map((async) => async());
+finalResolution = Promise.allSettled(promiseArr);
+finalResolution
+    .then((output) => {
+        for (let data of output) {
+            if (data.status === "fulfilled")
+                console.log(data.status + ": " + data.value);
+            else if (data.status == "rejected")
+                console.log(data.status + ": " + data.reason);
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
 //
 
 const asyncArr = [async1, async2, async3];
 const promiseArr = asyncArr.map((async) => async());
-finalResolution = Promise.all(promiseArr);
+finalResolution = Promise.allSettled(promiseArr);
 finalResolution
     .then((output) => {
         for (let data of output) {
-            console.log(data);
+            if (data.status === "fulfilled")
+                console.log(data.status + ": " + data.value);
+            else if (data.status == "rejected")
+                console.log(data.status + ": " + data.reason);
         }
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+        console.log(err);
+    });
+
 
     const asyncArr = [async1, async2, async3];
-    const promiseArr = asyncArr.map((async) => async());
-    (async function () {
-        try {
-            output = await Promise.all(promiseArr);
-            for (let data of output) {
-                console.log(data);
-            }
-        } catch (err) {
-            console.log(err);
+const promiseArr = asyncArr.map((async) => async());
+finalResolution = Promise.allSettled(promiseArr);
+finalResolution
+    .then((output) => {
+        for (let data of output) {
+            if (data.status === "fulfilled")
+                console.log(data.status + ": " + data.value);
+            else if (data.status == "rejected")
+                console.log(data.status + ": " + data.reason);
         }
-    })():
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 
     const asyncArr = [async1, async2, async3];
-    const promiseArr = asyncArr.map((async) => async());
-    finalResolution = Promise.all(promiseArr);
-    finalResolution
-        .then((output) => {
-            for (let data of output) {
-                console.log(data);
-            }
-        })
-        .catch ((err) => console.log(err));
-
-    const asyncArr = [async1, async2, async3];
-    const promiseArr = asyncArr.map((async) => async());
-    (async function () {
-        try {
-            output = await Promise.all(promiseArr);
-            for (let data of output) {
-                console.log(data);
-            }
-        } catch (err) {
-            console.log(err);
+const promiseArr = asyncArr.map((async) => async());
+finalResolution = Promise.allSettled(promiseArr);
+finalResolution
+    .then((output) => {
+        for (let data of output) {
+            if (data.status === "fulfilled")
+                console.log(data.status + ": " + data.value);
+            else if (data.status == "rejected")
+                console.log(data.status + ": " + data.reason);
         }
-    })();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
