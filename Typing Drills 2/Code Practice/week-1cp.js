e924e2944f43c798ac805a32766c833ad42deb5c
Asalagi <mypaintedfrog@yahoo.com>
2023-10-03 17:30:40 -0500

    attempt code prac

--- Typing Drills 2/Code Practice/week-1cp.js
+++ Typing Drills 2/Code Practice/week-1cp.js
@@ -6,3 +6,12 @@ function soluntion(str){
     return str.split('').reverse().join('');
 }
 
+// Practice Two - Implement a function, which takes a non-negative integer, 
+// representing the number of eggs to boil. It must return the time in minutes (integer), 
+// which it takes to have all the eggs boiled.
+
+function cookingTime(eggs) {
+    let minutes = Math.abs(5); // non negative num, 5 min is boiling time and math.abs makes positive num
+    let eggMax = 8; // 8 is the number of max eggs that can but boiled
+    return ; // head getting to mathy I don't know how to display what it wants
+}
