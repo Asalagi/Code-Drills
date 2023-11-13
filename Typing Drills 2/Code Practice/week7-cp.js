bf51216d012401a5350b66d64408f41205a23670
Asalagi <mypaintedfrog@yahoo.com>
2023-11-13 12:50:59 -0600

    cp5

--- Typing Drills 2/Code Practice/week7-cp.js
+++ Typing Drills 2/Code Practice/week7-cp.js
@@ -46,3 +46,58 @@ const quarterOf = (month) => {
     }
 }
 // All on my own! I'm sure there is a less wordy way of doing it, but I did it on my own and I'm proud that. YAY ME!
+
+
+// WEEK SEVEN DAY ONE - 11/13/2023
+
+function greet (name, owner) {
+  return name === owner ? 'Hello boss' : 'Hello guest';
+}
+
+
+const rps = (p1, p2) => {
+  if(p1 === 'scissors' && p2 === 'paper' || p1 === 'paper' && p2 === 'rock' || p1 === 'rock' && p2 === 'scissors'){
+    return 'Player 1 won!'
+  } else if(p1 === 'paper' && p2 === 'scissors' || p1 === 'rock' && p2 === 'paper' || p1 === 'scissors' && p2 === 'rock'){
+    return 'Player 2 won!'
+  } else {
+    return 'Draw!'
+  }
+};
+// did all on my own. Could have done p1 === p2 for draw.
+
+function rentalCarCost(d) {
+  let price = 40;
+  if (d >= 7){
+    return (d * price) - 50;
+  } else if (d < 7 && d >= 3){
+    return (d * price) - 20;
+  } else {
+    return d * price
+  }
+}
+
+
+function getGrade (s1, s2, s3) {
+  let average = (s1 + s2 + s3) / 3;
+  if (average >= 90 && average <= 100){
+    return 'A';
+  } else if (average >= 80 && average < 90){
+    return 'B';
+  } else if (average >= 70 && average < 80){
+    return 'C';
+  } else if (average >= 60 && average < 70){
+    return 'D';
+  } else {
+    return 'F';
+  }
+}
+
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+  }
+}
+
+
