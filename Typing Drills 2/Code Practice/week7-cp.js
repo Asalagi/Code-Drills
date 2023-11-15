85574bce442eb99303e67e45da19d293c201d491
Asalagi <mypaintedfrog@yahoo.com>
2023-11-15 14:14:20 -0600

    cp 8

--- Typing Drills 2/Code Practice/week7-cp.js
+++ Typing Drills 2/Code Practice/week7-cp.js
@@ -299,11 +299,6 @@ function correctTail(body, tail){
 
 
 function replace(s){
-  return s.replace('/[a,e,i,o,u]/gi', '!');
-}
-
-
-function replace(s){
   return s.replaceAll(/[aeiou]/gi, '!');
 }
 
@@ -324,3 +319,67 @@ function fizzbuzz(n){
   return fbArr;
 }
 // tried so very hard and was so very close, but I did need help
+
+
+let v1 = 50,
+    v2 = 100,
+    v3 = 150,
+    v4 = 200,
+    v5 = 2,
+    v6 = 250;
+
+function equal1(){
+  let a = v1,   
+      b = v1;   
+  return a + b;
+}
+
+function equal2(){
+  let a =  v4,
+      b =  v2; 
+  return a - b;
+}
+
+function equal3(){
+  let a =  v1, 
+      b =  v5; 
+  return a * b;
+}
+
+function equal4(){
+  let a =  v4, 
+      b =  v5; 
+  return a / b;
+}
+
+function equal5(){
+  let a =  v6, 
+      b =  v3; 
+  return a % b;
+}
+
+
+function position(letter){ 
+  return `Position of alphabet: ${letter.charCodeAt(letter) - 96}`;
+}
+
+
+function apple(x){
+  return Math.pow(x,2) > 1000 ? `It's hotter than the sun!!` : `Help yourself to a honeycomb Yorkie for the glovebox.`;
+}
+
+
+function weatherInfo (temp) {
+  let c = (temp - 32) * 5/9;
+  return c < 0 ? `${c} is freezing temperature` : `${c} is above freezing temperature`;
+}
+
+
+function remainder(n, m){
+  if(n > m){
+    return Math.round(n % m);
+  } else {
+    return Math.round(m % n);
+  }
+}
+// did all on my own YAY ME!
